9f5a418e0368f492aaad630e990390a5a42b16e7
andrejchenko <vera.andrejcenko@gmail.com>
2015-10-28 10:27:02 +0100

    Sparse unmixing result for lambda in the range: 0-0.001, step: 0.0001

--- unmixing/allEndMembersSparseUnmixing.m
+++ unmixing/allEndMembersSparseUnmixing.m
@@ -1,6 +1,7 @@
 %% Use all training pixels as endmembers for each class: E = trainData
 %  Enforce sparsity in the unmixing by using sparse unmixing method: sunsal 
 %  which controls the sparsity with the lambda parameter
+%  trying to see if Matlab will create the .asv file 
 
 function allEndMembersSparseUnmixing()
     load('svmClassification_15_10_2015_40_train_60_test_10_cv');
@@ -8,7 +9,7 @@ function allEndMembersSparseUnmixing()
     
     accuracies = [];
     x = [];
-    for lambda = 0:0.1:1
+    for lambda = 0:0.0001:0.001
         alphas = sunsal(E,testData','Positivity','yes','addone','no','lambda',lambda);
         %save alphaAllTrainData alphas
         %load alphaAllTrainData
@@ -31,4 +32,10 @@ function allEndMembersSparseUnmixing()
     plot(x,accuracies,'--gs','LineWidth',2,'MarkerSize',10,'MarkerEdgeColor','b','MarkerFaceColor',[0.3,0.3,0.3]);
     stop = 1;
 end
+
+%% for low numbers of lambda: lambda = 0:0.01:0.1
+%  Error in the sunsal method:
+%  Undefined function or variable 'soft'.
+%  Error in sunsal (line 341)
+%  z =  soft(x-d,lambda/mu);
 
